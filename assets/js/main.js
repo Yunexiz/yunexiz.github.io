9c69961cb5618b39f5a374155c2697f41bd38c8e
Yosuke Nishiyama <141266522+Yunexiz@users.noreply.github.com>
2025-12-06 16:00:39 +0000

    feat(script): use await to avoid observer race condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,30 +7,28 @@ import { displayProjects } from "./projects.js";
 
 checkNavPlacement();
 animate();
-displayProjects();
+await displayProjects();
+await setProfileImage(urlParams);
 
-// Start entry animations after loading profile
-setProfileImage(urlParams).then(() => {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        if (entry.target.tagName === "UL" || entry.target.tagName === "OL") {
-          entry.target.classList.remove("entry-animation");
-          const listItems = entry.target.querySelectorAll("li");
-          listItems.forEach((item, index) => {
-            setTimeout(() => {
-              item.classList.remove("entry-animation");
-              item.classList.add("visible");
-            }, 300 * index);
-          });
-        } else {
-          entry.target.classList.remove("entry-animation");
-          entry.target.classList.add("visible");
-        }
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      if (entry.target.tagName === "UL" || entry.target.tagName === "OL") {
+        entry.target.classList.remove("entry-animation");
+        const listItems = entry.target.querySelectorAll("li");
+        listItems.forEach((item, index) => {
+          setTimeout(() => {
+            item.classList.remove("entry-animation");
+            item.classList.add("visible");
+          }, 300 * index);
+        });
+      } else {
+        entry.target.classList.remove("entry-animation");
+        entry.target.classList.add("visible");
       }
-    });
+    }
   });
-  document
-    .querySelectorAll(".entry-animation")
-    .forEach((el) => observer.observe(el));
 });
+document
+  .querySelectorAll(".entry-animation")
+  .forEach((el) => observer.observe(el));
